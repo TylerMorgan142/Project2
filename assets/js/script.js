51f8eb23105391a9362f0e47667fe283c7717ee4
TylerMorgan142 <tylermorgan142@gmail.com>
2023-03-28 21:47:04 +0000

    Added javascript skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded");
+
+function runGame() {
+
+}
+
+function rock() {
+
+}
+
+function paper() {
+
+}
+
+function scissors() {
+
+}
+
+function lizard(){
+
+}
+
+function spock(){
+
+}
+
+function incrementPlayerScore() {
+
+}
+
+function incrementComputerScore() {
+
+}
