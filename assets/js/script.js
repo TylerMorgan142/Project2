bd5f51d381b850b34bbad8e1f9f98cb6bd4fd9d7
TylerMorgan142 <tylermorgan142@gmail.com>
2023-03-29 23:53:59 +0000

    Added runGame and ComputerChoice functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,87 @@
-document.addEventListener("DOMContentLoaded");
+// Waits for the DOM to finish loading befure running the game
+// Gets the button elements and adds event listeners to them
 
-function runGame() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let playerChoice = this.getAttribute ("data-type");
+            runGame(playerChoice);
+        })
+    }
+})
+
+/**
+ * Determines the players choice of rock, paper, scissors, lizard or spock
+ * Runs the relevant function based on the players choice
+ */
+
+function runGame(playerChoice) {
+    if (playerChoice === "rock") {
+        playRock();
+    } else if (playerChoice === "paper") {
+        playPaper();
+    } else if (playerChoice === "scissors") {
+        playScissors();
+    } else if (playerChoice === "lizard") {
+        playLizard()
+    } else if (playerChoice === "spock") {
+        playSpock()
+    } else {
+        alert(`Unknown player choice ${playerChoice}`);
+        throw `Unkown player choice ${playerChoice}. Aborting!`;
+    }
+    computerChoice()
+}
+
+/**
+ * Determines the computers choice of rock, paper, scissors, lizard or spock by generating a random number between 1 and 5
+ */
+
+function computerChoice() {
+    let getcomputerChoice = Math.floor(Math.random() * 5) + 1;
+    if (getcomputerChoice === 1) {
+     document.getElementById("computer-image").src="assets/images/rock.png";
+    } else if (getcomputerChoice === 2) {
+        document.getElementById("computer-image").src="assets/images/paper.png" ;
+       } else if (getcomputerChoice === 3) {
+        document.getElementById("computer-image").src="assets/images/scissors.png" ;
+       } else if (getcomputerChoice === 4) {
+        document.getElementById("computer-image").src="assets/images/lizard.png" ;
+       } else if (getcomputerChoice === 5) {
+        document.getElementById("computer-image").src="assets/images/spock.png" ;
+       } 
+ }
+
+
+function playRock() {
+    
+   
+}
+
+function playPaper() {
 
 }
 
-function rock() {
+function playScissors() {
 
 }
 
-function paper() {
+function playLizard(){
 
 }
 
-function scissors() {
+function playSpock(){
 
 }
 
-function lizard(){
 
-}
-
-function spock(){
-
-}
 
 function incrementPlayerScore() {
-
+    
 }
 
 function incrementComputerScore() {
-
+    
 }
